061a7413e2f544d25b64c59e5141db99960b14f5
shimomo <yuichi@shimomo.net>
2025-08-30 18:43:40 +0900

    chore: fix typo

--- scraper.php
+++ scraper.php
@@ -16,22 +16,22 @@ $version = $argv[1] ?? 'v2';
 // 本日の日付を東京時間で取得
 $date = Carbon::today('Asia/Tokyo');
 
-// v2 の場合のみ ResultScraper を利用して直前情報データを取得
+// v2 の場合のみ ResultScraper を利用して結果データを取得
 if ($version === 'v2') {
     $scraperInstance = Scraper::getInstance();
     $scraperAdapter = new ScraperAdapter($scraperInstance);
     $scraper = new ResultScraper($scraperAdapter);
 
-    // 指定日付の直前情報データをスクレイピング
+    // 指定日付の結果データをスクレイピング
     $results = $scraper->scrape($date);
 }
 
-// 直前情報データが取得できなかった場合は処理終了
+// 結果データが取得できなかった場合は処理終了
 if (empty($results ?? [])) {
     exit;
 }
 
-// 直前情報データを JSON ファイルとして保存
+// 結果データを JSON ファイルとして保存
 // 日付付きの JSON ファイルとして保存（例: docs/{version}/YYYY/YYYYMMDD.json）
 // 最新データとして today.json にも保存
 $saver = new ResultSaver();
