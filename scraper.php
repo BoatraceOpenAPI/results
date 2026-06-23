7da9a8a0cf6e847574e890218e4e63b3ff4e3335
@lamrongol <lamrongol@gmail.com>
2026-06-24 00:24:36 +0900

    過去データ取得ワークフロー追加

--- scraper.php
+++ scraper.php
@@ -12,9 +12,17 @@ use Carbon\CarbonImmutable as Carbon;
 
 // コマンドライン引数からバージョンを取得（デフォルトは v3）
 $version = $argv[1] ?? 'v3';
+$day_specified = ($argc == 3 && $argv[2] == 'True');
 
-// 本日の日付を東京時間で取得
+// 昨日の日付を東京時間で取得
 $date = Carbon::yesterday('Asia/Tokyo');
+if ($day_specified) {
+    $date = new Carbon(file_get_contents("docs/{$version}/recorded_day.json"))->timezone('Asia/Tokyo');
+    $date = $date->subDay();
+    if ($date->isBefore(Carbon::parse('2020-01-01'))) {
+        exit;
+    }
+}
 
 // v2 or v3 の場合のみ ResultScraper を利用して結果データを取得
 if ($version === 'v2' || $version === 'v3') {
@@ -36,4 +44,9 @@ if (empty($results ?? [])) {
 // 最新データとして yesterday.json にも保存
 $saver = new ResultSaver();
 $saver->save($results, "docs/{$version}/" . $date->format('Y') . '/' . $date->format('Ymd') . '.json');
-$saver->save($results, "docs/{$version}/yesterday.json");
+if ($day_specified) {
+    file_put_contents("docs/{$version}/recorded_day.json", $date->toDateString());
+} else {
+    $saver->save($results, "docs/{$version}/yesterday.json");
+}
+
